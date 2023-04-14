6e5813618aa67ce77a887f539c54140437173411
Joshua Reynolds <j.reynolds0962@gmail.com>
2023-04-14 10:42:43 -0500

    weather parsed

--- retreive_weather.py
+++ retreive_weather.py
@@ -1,6 +1,8 @@
 import requests
 import cped
 
+#33.44
+#-94.04
 
 class get_weather():
     def __init__(self, key, url, city, days=7, units="imperial"):
@@ -21,9 +23,12 @@ base_url = "http://api.openweathermap.org/data/2.5/forecast/daily"
 # print("Warning: City list does not include all cities in the world")
 # city = input("Enter a city name: ")
 
-print("Now lets get the coordinates")
-lat = input("whats the latitude?: ")
-lon = input("whats the longitude?: ")
+#print("Now lets get the coordinates")
+#lat = input("whats the latitude?: ")
+#lon = input("whats the longitude?: ")
+
+lat = "33.44"
+lon = "-94.04"
 
 exclusion = "current,minutely,hourly,alerts"
 
@@ -37,13 +42,21 @@ response = requests.get(request_url)
 if response.status_code == 200:
     data = response.json()
     #weather = data['weather'][0]['description']
-    #print(f"We're looking at a {weather} day")
-    #temperature = round((data['main']['temp'] - 273.15), 2)
-    #print(f"the temperature in {city} is {temperature} celcius")
+    print("Retrieved successfully")
 
-
-    print(data)
 else:
     print("An error occured")
     print(response.status_code)
     print(response.json())
+
+
+weather = []
+
+for item in data['daily']:
+    full_day = []
+    full_day.append(item['dt'])
+    full_day.append(item['temp']['min'])
+    full_day.append(item['temp']['max'])
+    weather.append(full_day)
+
+print(weather)
